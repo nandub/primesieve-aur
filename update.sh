d74d7dca2fcd8a911e3905bac5a0f060cd7cea13
Fernando Ortiz <fortiz2k@gmail.com>
2020-01-09 11:30:47 -0600

    use makepkg if available, and other enhancements

--- update.sh
+++ update.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-if ! [ -f pkgsum.sh ]; then
-  wget https://gist.github.com/nandub/04f1f049a5e55476b642/raw/02c13a02fb7e9121d1390b3a4f5f6fca5d11e07b/pkgsum.sh
-  chmod +x pkgsum.sh
-fi
-
 get_latest() {
   wget -q -O- 'https://github.com/kimwalisch/primesieve/wiki/Downloads' | \
   sed -n 's,.*primesieve-\([0-9][^>]*\)\.tar.*,\1,p' | \
@@ -22,7 +17,11 @@ if ! [ "$latest_version" = "$current_version" ]; then
   sed -i.bak "s/$current_version/$latest_version/g" PKGBUILD
   sed -i.bak "s/pkgrel=[^d.]/pkgrel=1/g" PKGBUILD
   ./pkgsum.sh
-  mksrcinfo
+  if which makepkg &> /dev/null; then
+    makepkg --printsrcinfo > .SRCINFO
+  else
+    mksrcinfo
+  fi
 else
   echo Nothing to update.
 fi
